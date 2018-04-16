9cb737b1803dc44173b3b3d09db84823c179f9c4
tommag <magnier.tom@gmail.com>
2018-04-16 12:32:18 +0200

    Serial API - first commit

--- STM32_KXKM_Battery_monitoring/STM32_KXKM_Battery_monitoring.ino
+++ STM32_KXKM_Battery_monitoring/STM32_KXKM_Battery_monitoring.ino
@@ -21,7 +21,12 @@ The processor serial port is available on the ESP32 programmation connector. RX 
 Tom Magnier - 04/2018
 */
 
+//TODO Watchdog
+//TODO state machine : release bouton, activation esp, activation load sw, attente section critique, etc
+//TODO display single led when on
+
 #include <AceButton.h>
+#include "KXKM_STM32_energy_API.h"
 
 // Hardware definitions
 const uint8_t LED_PINS[] = {4,3,2,1};
@@ -41,6 +46,7 @@ const unsigned long STARTUP_GUARD_TIME_MS = 5000; // Ignore long presses during 
 const unsigned long BATT_DISPLAY_TIME_MS = 3000; // Display the battery level during this time then shut down the LEDs
 
 ace_button::AceButton button(PUSH_BUTTON_DETECT_PIN, LOW);
+KXKM_STM32_Energy::PushButtonEvent buttonEvent = KXKM_STM32_Energy::NO_EVENT;
 unsigned long battLevelDisplayStart;
 
 #define SERIAL_DEBUG(str) \
@@ -59,9 +65,12 @@ void setup() {
 
   pinMode(ESP32_TX_PIN, INPUT); // Switch TX to High Z (shared with ESP32 programmation connector)
 
+  Serial1.setTimeout(10);
+
   ace_button::ButtonConfig* buttonConfig = button.getButtonConfig();
   buttonConfig->setEventHandler(handleButtonEvent);
   buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureClick);
+  buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureDoubleClick);
   buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureLongPress);
 
   // To keep interactions consistent, a long press is required to start up the board.
@@ -73,9 +82,9 @@ void setup() {
   //TODO check battery voltage & shut down if too low
 
   // Power up the board
-  digitalWrite(POWER_ENABLE_PIN, HIGH); //Keep 3.3V regulator enabled
-  digitalWrite(MAIN_OUT_ENABLE_PIN, HIGH);
-  digitalWrite(ESP32_ENABLE_PIN, HIGH);
+  set3V3RegState(true); //Keep 3.3V regulator enabled
+  setLoadSwitchState(true); //TODO after 2s if no disable command has been received
+  setESP32State(true); //Enable ESP32
 
   initLedGauge();
 
@@ -108,6 +117,11 @@ void handleButtonEvent(ace_button::AceButton* button, uint8_t eventType, uint8_t
     case ace_button::AceButton::kEventClicked:
       //Display the battery percentage on the LED gauge
       battLevelDisplayStart = millis();
+      buttonEvent = KXKM_STM32_Energy::BUTTON_CLICK_EVENT;
+      break;
+
+    case ace_button::AceButton::kEventDoubleClicked:
+    buttonEvent = KXKM_STM32_Energy::BUTTON_DOUBLE_CLICK_EVENT;
       break;
 
     case ace_button::AceButton::kEventLongPressed:
--- STM32_KXKM_Battery_monitoring/serial.ino
+++ STM32_KXKM_Battery_monitoring/serial.ino
@@ -6,6 +6,7 @@
  * so the TX line must be released (switched to High-Z) after each communication to
  * leave the programmation connector available.
  */
+#include "KXKM_STM32_energy_API.h"
 
 void beginSerial()
 {
@@ -16,4 +17,90 @@ void endSerial()
 {
   Serial1.flush();
   pinMode(ESP32_TX_PIN, INPUT); //Set TX pin to Hi Z to allow ESP32 programmation from external connector
+}
+
+void serialEvent1()
+{
+  if (Serial1.find(KXKM_STM32_Energy::PREAMBLE))
+  {
+    char cmd = Serial1.read();
+    long arg = 0;
+
+    if (KXKM_STM32_Energy::hasArgument((KXKM_STM32_Energy::CommandType)cmd))
+      arg = Serial1.parseInt();
+
+    switch (cmd)
+    {
+      case KXKM_STM32_Energy::GET_BATTERY_VOLTAGE:
+        sendAnswer(_avgBattVoltage);
+        break;
+
+      case KXKM_STM32_Energy::GET_BATTERY_PERCENTAGE:
+        sendAnswer(getBatteryPercentage());
+        break;
+
+      case KXKM_STM32_Energy::SET_LEDS:
+        for (int i = 0; i < 6; i++)
+        {
+          setSingleLed(i, arg % 10);
+          arg /= 10;
+        }
+        break;
+
+      case KXKM_STM32_Energy::SET_LED_GAUGE:
+        setLedGaugePercentage(arg);
+        break;
+
+      case KXKM_STM32_Energy::SET_LOAD_SWITCH:
+        //TODO if not set disable autoset
+        setLoadSwitchState(arg > 0);
+        break;
+
+      case KXKM_STM32_Energy::SHUTDOWN:
+        //TODO leave critical section ?
+        shutdown();
+        break;
+
+      case KXKM_STM32_Energy::REQUEST_RESET:
+        setESP32State(false);
+        delay(10);
+        setESP32State(true);
+        break;
+
+      case KXKM_STM32_Energy::SET_BATTERY_VOLTAGE_LOW:
+      case KXKM_STM32_Energy::SET_BATTERY_VOLTAGE_1:
+      case KXKM_STM32_Energy::SET_BATTERY_VOLTAGE_2:
+      case KXKM_STM32_Energy::SET_BATTERY_VOLTAGE_3:
+      case KXKM_STM32_Energy::SET_BATTERY_VOLTAGE_4:
+      case KXKM_STM32_Energy::SET_BATTERY_VOLTAGE_5:
+      case KXKM_STM32_Energy::SET_BATTERY_VOLTAGE_6:
+        _battVoltageBreaks[cmd - KXKM_STM32_Energy::SET_BATTERY_VOLTAGE_LOW] = arg;
+        break;
+
+      case KXKM_STM32_Energy::ENTER_CRITICAL_SECTION:
+        //TODO
+        //TODO max timeout
+        break;
+
+      case KXKM_STM32_Energy::LEAVE_CRITICAL_SECTION:
+        //TODO
+        break;
+
+      case KXKM_STM32_Energy::GET_BUTTON_EVENT:
+        sendAnswer((int)buttonEvent);
+        buttonEvent = KXKM_STM32_Energy::NO_EVENT;
+        break;
+
+      default:
+        break;
+    };
+  }
+}
+
+void sendAnswer(int value)
+{
+  beginSerial();
+  Serial1.write(KXKM_STM32_Energy::PREAMBLE);
+  Serial1.println(value);
+  endSerial();
 }
