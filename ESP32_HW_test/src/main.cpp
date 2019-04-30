efb3c2ac70468aebb1877ff8aeee2645681f544c
tommag <magnier.tom@gmail.com>
2019-04-30 09:43:38 +0200

    Add v2 pinout to ESP32 test code

--- ESP32_HW_test/src/main.cpp
+++ ESP32_HW_test/src/main.cpp
@@ -12,11 +12,38 @@ AudioGeneratorMP3 *mp3;
 AudioFileSourceSD *file;
 AudioOutputI2S *out;
 
+#if HW_REVISION == 1
+  const uint8_t I2C_SDA_PIN = 2;
+  const uint8_t I2C_SCL_PIN = 4;
+  const uint8_t I2S_LRCK_PIN = 27;
+  const uint8_t I2S_DATA_PIN = 26;
+  const uint8_t I2S_BCK_PIN = 25;
+  const uint8_t SD_DI_PIN = 23;
+  const uint8_t SD_DO_PIN = 19;
+  const uint8_t SD_SCK_PIN = 18;
+  const uint8_t SD_CS_PIN = 5;
+
+#elif HW_REVISION == 2
+  const uint8_t I2C_SDA_PIN = 32;
+  const uint8_t I2C_SCL_PIN = 33;
+  const uint8_t I2S_LRCK_PIN = 25;
+  const uint8_t I2S_DATA_PIN = 26;
+  const uint8_t I2S_BCK_PIN = 27;
+  const uint8_t SD_DI_PIN = 19;
+  const uint8_t SD_DO_PIN = 5;
+  const uint8_t SD_SCK_PIN = 18;
+  const uint8_t SD_CS_PIN = 21;
+
+#else
+  #error "HW_REVISION undefined or invalid. Should be 1 or 2"
+#endif
+
+
 PCM51xx pcm(Wire); //Using the default I2C address 0x74
 
 bool sd_setup() {
-  // CS / SS  GPIO for SD module
-  if (!SD.begin(5)) {
+  SPI.begin(SD_SCK_PIN, SD_DO_PIN, SD_DI_PIN);
+  if (!SD.begin(SD_CS_PIN)) {
     Serial.println("SD card error");
     return false;
   }
@@ -38,12 +65,12 @@ void setup()
 
   // DAC start
   out = new AudioOutputI2S();
-  out->SetPinout(25, 27, 26); //HW dependent ! BCK, LRCK, DATA
+  out->SetPinout(I2S_BCK_PIN, I2S_LRCK_PIN, I2S_DATA_PIN); 
   out->SetBitsPerSample(16);
   out->SetRate(44100);
 
   // PCM5141 start
-  Wire.begin(2, 4); //HW dependent ! SDA, SCL
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN); 
 
   if (pcm.begin(PCM51xx::SAMPLE_RATE_44_1K, PCM51xx::BITS_PER_SAMPLE_16))
     Serial.println("PCM51xx initialized successfully.");
