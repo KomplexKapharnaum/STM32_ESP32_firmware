28484c62bc2b13eb6e147b085b61edf34ac1e7d6
richard fontaine <ririfonfon10@gmail.com>
2021-03-04 17:53:52 +0100

    Create hw_control.h

---
+++ VS_STM32_KXKM_Battery_monitoring/src/hw_control.h
@@ -0,0 +1,29 @@
+/* Hardware control functions (power state of different board components) */
+
+/* 3.3V regulator enable. If set to false, the whole board will be shutdown (
+including this microcontroller) */
+void set3V3RegState(bool state)
+{
+  digitalWrite(POWER_ENABLE_PIN, state);
+}
+
+/* Main out load switch */
+void setLoadSwitchState(bool state)
+{
+  digitalWrite(MAIN_OUT_ENABLE_PIN, state);
+}
+
+/* ESP32 enable */
+void setESP32State(bool state)
+{
+  digitalWrite(ESP32_ENABLE_PIN, state);
+}
+
+/* Full shutdown sequence */
+void shutdown()
+{
+  clearLeds();
+  setLoadSwitchState(false);
+  setESP32State(false);
+  pinMode(POWER_ENABLE_PIN, INPUT); //Bye bye !
+}
