cab9259081ebe998b42afc9877796d15395cc3a1
richard fontaine <ririfonfon10@gmail.com>
2021-03-04 17:54:13 +0100

    Create wdt.h

---
+++ VS_STM32_KXKM_Battery_monitoring/src/wdt.h
@@ -0,0 +1,29 @@
+/* Watchdog timer functions */
+#include "stm32f0xx_hal_iwdg.h"
+
+IWDG_HandleTypeDef IwdgHandle;
+
+/* Init watchdog with a 250ms period */
+void initWatchdog()
+{
+  IwdgHandle.Instance = IWDG;
+
+  /* Set counter reload value to obtain 250ms IWDG TimeOut.
+     LSI Frequency : around 40kHz
+     IWDG counter clock Frequency = LsiFreq / 32 => ~ 1250Hz
+     Counter Reload Value = 250ms / IWDG counter clock period
+                          = 0.25s / (32/LsiFreq)
+                          = LsiFreq / (32 * 4)
+                          = LsiFreq / 128 */
+  IwdgHandle.Init.Prescaler = IWDG_PRESCALER_32;
+  //IwdgHandle.Init.Reload    = uwLsiFreq/128;
+  IwdgHandle.Init.Reload    = 312; //40000 / 128
+  IwdgHandle.Init.Window = IWDG_WINDOW_DISABLE;
+
+  HAL_IWDG_Init(&IwdgHandle);
+}
+
+void refreshWatchdog()
+{
+  HAL_IWDG_Refresh(&IwdgHandle);
+}
