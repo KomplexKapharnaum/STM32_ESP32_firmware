99b0a347f9e5fc7f898c4b828679cb0f080a4151
richard fontaine <ririfonfon10@gmail.com>
2021-12-06 12:38:11 +0100

    Update board_id.h

--- STM32_KXKM_Battery_monitoring/board_id.h
+++ STM32_KXKM_Battery_monitoring/board_id.h
@@ -4,6 +4,6 @@
 /* Board ID definition. Set it here if you compile using Arduino.
  * If you compile and flash with the Makefile it will be updated later when uploading.
  */
-uint32_t __attribute__((section (".boardId"))) BOARD_ID = 1;
+uint32_t __attribute__((section (".boardId"))) BOARD_ID = 201;
 
 #endif
